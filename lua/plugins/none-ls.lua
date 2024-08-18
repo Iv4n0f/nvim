4528455eacb11989c89f6cb19bfcafb75fc6a570
Iv4n0f <ivanalexanderlopezzegarra@gmail.com>
2024-08-18 14:08:03 -0500

    autoinstall formatters

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,22 +1,37 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        -- Instalar formatters con :Mason y agregarlos a este archivo
-        -- reemplazar "_" por "-"
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.clang_format,
-        null_ls.builtins.formatting.pretty_php,
-        null_ls.builtins.formatting.black,
-        -- rust-analyzer ya tiene una funcion de formateo integrada
-        -- no es necesario añadirla aquí
-      },
-    })
+  {
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "nvimtools/none-ls.nvim",
+    },
+    config = function()
+      require("mason-null-ls").setup({
+        ensure_installed = { "stylua", "prettier", "clang_format", "pretty_php", "black" },
+      })
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
+      null_ls.setup({
+        sources = {
+          -- Instalar formatters con :Mason y agregarlos a este archivo
+          -- reemplazar "_" por "-"
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.prettier,
+          null_ls.builtins.formatting.clang_format,
+          null_ls.builtins.formatting.pretty_php,
+          null_ls.builtins.formatting.black,
+          -- rust-analyzer ya tiene una funcion de formateo integrada
+          -- no es necesario añadirla aquí
+        },
+      })
 
-    -- Dar formato al buffer actual
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+      -- Dar formato al buffer actual
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
+  },
 }
