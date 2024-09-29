79f8991d742ac2af0166d4f4f685b3a21f97fff6
Iv4n0f <ilopezz@unsa.edu.pe>
2024-09-28 20:42:09 -0500

    session y wl-clipboard para wayland systems

---
+++ lua/plugins/session.lua
@@ -0,0 +1,5 @@
+return { "tpope/vim-obsession" }
+-- Se puede crear sesiones con :Obsession
+
+-- :O funciona tambien
+-- para inciar la sesion $nvim -S
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,27 +2,28 @@
 -- KDE tiene esta opción en las configuraciones
 -- para sesiones X11 ->  setxkbmap -option "caps:escape"
 
---Tabzise
+-- Tabzise
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
---Clipboard por defecto a clipboard del sistema(xclip required)
+-- Clipboard por defecto a clipboard del sistema
+-- para X11 instalar xclip, para wayland wl-clipboard
 vim.cmd("set clipboard=unnamedplus")
 
---Desactivar el mouse
+-- Desactivar el mouse
 vim.cmd("set mouse = ")
 
---Leader key en la barra espaciadora
+-- Leader key en la barra espaciadora
 vim.g.mapleader = " "
 vim.opt.swapfile = false
 vim.opt.scrolloff = 99
 
---Dejar de resaltar la búsqueda "/"
+-- Dejar de resaltar la búsqueda "/"
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 
---Copiar todo el archivo(buffer) actual al clipboard por defecto
+-- Copiar todo el archivo(buffer) actual al clipboard por defecto
 vim.keymap.set("n", "<leader>ya", "ggVGy<C-o>")
 
 --  al presionar {{ en insert mode se crea esta estructura
@@ -31,10 +32,10 @@ vim.keymap.set("n", "<leader>ya", "ggVGy<C-o>")
 --  }
 vim.keymap.set("i", "{{", "{<CR>}<Esc>O")
 
---Evitar dolores de cabeza
+-- Evitar dolores de cabeza
 vim.cmd("nnoremap q: <nop>")
 vim.cmd("nnoremap Q <nop>")
 
---Numeros de linea abolsutos y relativos
+-- Numeros de linea abolsutos y relativos
 vim.wo.number = true
 vim.wo.relativenumber = true
